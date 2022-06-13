00dd028f0167b0e00d7cd4c679d4544090d3a575
Marcos Nikel <marcosnikeldev@gmail.com>
2022-06-13 14:09:40 -0300

    main js file

---
+++ main.js
@@ -0,0 +1,9 @@
+//Main file Javascript Web
+/*JS in console in Google Chrome - - - > 
+-> query selector -> search for some selector
+querySelector('') you need to tell where to find 
+
+document.
+*/
+
+document.querySelector('.tecla_pom');
